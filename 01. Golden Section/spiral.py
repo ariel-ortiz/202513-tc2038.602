98fc0b3e675eded69da26f5e513822583dc7e281
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-08-25 13:31:09 -0600

    Add implementation of golden spiral using turtle graphics

--- 01. Golden Section/spiral.py
+++ 01. Golden Section/spiral.py
@@ -1,4 +1,8 @@
 import turtle
+from math import sqrt
+
+
+PHI: float = 2 / (sqrt(5) - 1)
 
 
 def square(size: int) -> None:
@@ -7,6 +11,22 @@ def square(size: int) -> None:
         turtle.left(90)
 
 
+def golden_spiral(n: int) -> None:
+    size: float = 10
+    for _ in range(n):
+        turtle.pencolor('blue')
+        square(int(size))
+        turtle.pencolor('red')
+        turtle.circle(size, 90)
+        size *= PHI
+
+
 if __name__ == '__main__':
-    square(300)
+    # square(300)
+    # turtle.circle(100, 45)
+    turtle.hideturtle()
+    turtle.pensize(3)
+    turtle.speed('fastest')
+    golden_spiral(9)
     turtle.done()
+    print(PHI)
