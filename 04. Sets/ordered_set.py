71e9c45d8d32ec2b6d7eb820541345b29b761d86
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-09-08 14:57:20 -0600

    Refactor OrderedSet: improve iteration, equality, and comparison methods; add discard and intersection functionality

--- 04. Sets/ordered_set.py
+++ 04. Sets/ordered_set.py
@@ -30,7 +30,7 @@ class OrderedSet[T]:
     # Complexity: O(N)
     def __iter__(self) -> Iterator[T]:
         current: _Node[T] = self.__sentinel.next
-        while current != self.__sentinel:
+        while current is not self.__sentinel:
             yield cast(T, current.info)
             current = current.next
 
@@ -60,23 +60,48 @@ class OrderedSet[T]:
         self.__sentinel.prev.next = new_node
         self.__sentinel.prev = new_node
 
+    # Complexity: O(N*M), where N = len(self) and M = len(other)
+    def __eq__(self, other: object) -> bool:
+        if self is other:
+            return True
+        if not isinstance(other, OrderedSet):
+            return False
+        if len(self) != len(other):  # type: ignore
+            return False
+        for elem in self:
+            if elem not in other:
+                return False
+        return True
+
+    def __le__(self, other: OrderedSet[T]) -> bool:
+        if self is other:
+            return True
+        if len(self) > len(other):
+            return False
+        for elem in self:
+            if elem not in other:
+                return False
+        return True
+
+    def discard(self, value: T) -> None:
+        current: _Node[T] = self.__sentinel.next
+        while current is not self.__sentinel:
+            if current.info == value:
+                current.prev.next = current.next
+                current.next.prev = current.prev
+                self.__count -= 1
+                return
+            current = current.next
+
+    def __and__(self, other: OrderedSet[T]) -> OrderedSet[T]:
+        result: OrderedSet[T] = OrderedSet()
+        for elem in self:
+            if elem in other:
+                result.add(elem)
+        return result
+
 
 if __name__ == '__main__':
-    s: OrderedSet[int] = OrderedSet()
-    print(s)
-    s.add(4)
-    s.add(8)
-    s.add(15)
-    s.add(42)
-    s.add(108)
-    print(len(s))
-    print(s)
-    print(list(s))
-    print(108 in s)
-    print(107 in s)
-    b: OrderedSet[str] = OrderedSet('hello')
-    print(b)
-    c = b
-    c.add('x')
-    print(b)
-    print(c)
+    s1: OrderedSet[int] = OrderedSet([4, 8, 15, 16])
+    s2: OrderedSet[int] = OrderedSet([5, 6, 8, 16, 4])
+    print(f'{s1 & s2 = }')
