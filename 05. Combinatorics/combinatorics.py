aae3d48b293f66b2257416e8cce30db602fc0401
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-09-25 14:38:01 -0600

    Add combinations, insert, insert_everywhere, permute, and permutations functions to combinatorics module

--- 05. Combinatorics/combinatorics.py
+++ 05. Combinatorics/combinatorics.py
@@ -5,11 +5,36 @@ def nicely_sorted[T](s: list[list[T]]) -> list[list[T]]:
 
     return sorted(s, key=size_and_content)
 
+
 def power_set[T](s: list[T]) -> list[list[T]]:
     if not s:
         return [[]]
     recursive: list[list[T]] = power_set(s[:-1])
     return recursive + [e + [s[-1]] for e in recursive]
+
+
+def combinations[T](s: list[T], k: int) -> list[list[T]]:
+    return [t for t in power_set(s) if len(t) == k]
+
+
+def insert[T](x: T, s: list[T], i: int) -> list[T]:
+    return s[:i] + [x] + s[i:]
+
+
+def insert_everywhere[T](x: T, s: list[T]) -> list[list[T]]:
+    return [insert(x, s, i) for i in range(len(s) + 1)]
+
+
+def permute[T](s: list[T]) -> list[list[T]]:
+    if not s:
+        return [[]]
+    empty: list[list[T]] = []
+    return sum([insert_everywhere(s[-1], e) for e in permute(s[:-1])], empty)
+
+
+def permutations[T](s: list[T], k: int) -> list[list[T]]:
+    empty: list[list[T]] = []
+    return sum([permute(e) for e in combinations(s, k)], empty)
 
 
 if __name__ == '__main__':
@@ -20,3 +45,13 @@ if __name__ == '__main__':
     pprint(nicely_sorted(power_set(['a', 'b', 'c'])))
     pprint(nicely_sorted(power_set(['a', 'b', 'c', 'd'])))
     pprint(nicely_sorted(power_set(['a', 'b', 'c', 'd', 'e'])))
+    pprint(combinations(['a', 'b', 'c'], 2))
+    pprint(combinations(['a', 'b', 'c', 'd'], 4))
+    pprint(combinations(['a', 'b', 'c', 'd'], 1))
+    pprint(combinations(['a', 'b', 'c', 'd', 'e'], 4))
+    print(insert(5, [1, 2, 3], 0))
+    print(insert(5, [1, 2, 3], 2))
+    print(insert(5, [1, 2, 3], 3))
+    print(insert_everywhere(5, [1, 2, 3]))
+    pprint(nicely_sorted(permute([1, 2, 3, 4])))
+    pprint(nicely_sorted(permutations([1, 2, 3, 4], 2)))
